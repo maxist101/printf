69b9f46847cfb8d82fb2fa9475cf6ecafb9320d8
domgeorge <obinnadom007@gmail.com>
2023-09-17 06:19:34 -0700

    Rewrote and updated it

--- printf_unsigned.c
+++ printf_unsigned.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
  * printf_unsigned - prints integer
  * @args: argument to print
  * Return: number of characters printed
  */
+
 int printf_unsigned(va_list args)
 {
 	unsigned int n = va_arg(args, unsigned int);
